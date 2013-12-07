711a4252c47184455ba5b0627d09eed591021476
shry15harsh <Just for e@GotIt.(none)>
2013-12-07 21:15:31 +0530

    'resultPage'

--- js/dynamicGrid.js
+++ js/dynamicGrid.js
@@ -4,16 +4,16 @@
 	var originalColor=["rgb(140,0,0)","rgb(150,0,0)","rgb(160,0,0)","rgb(170,0,0)","rgb(180,0,0)","rgb(190,0,0)","rgb(200,0,0)","rgb(210,0,0)","rgb(220,0,0)","rgb(230,0,0)","rgb(240,0,0)","rgb(250,0,0)"];
 	var isFaded=[false,false,false,false,false,false,false,false,false,false,false,false];
 	var percent=["8.3333333332%","16.666666666666664%","25%","33.33333333333333%","41.66666666666667%","50%","58.333333333333336%","66.66666666666666%","75%","83.33333333333334%","91.66666666666666%","100%"];
-	var blockNo;
-	var blockCount=0;
-	var totalBlocks=12;
-	var HidingId;
+	var blockNo;		//Tells the current block number
+	var blockCount=0;	//Tells the block hidden count during hiding process
+	var totalBlocks=12;	//Tells total number of blocks
+	var HidingId;		//Id of setInterval to stop it after all blocks are hidden
 	var i=0;
 	function makeHide(){
 		if(blockCount==totalBlocks){
 			clearInterval(HidingId);
 			for(i=0;i<12;i++){
-				color[i]=originalColor[i];
+				color[i]=originalColor[i];	//Copying orginal colors back
 				isFaded[i]=false;
 			}
 			i=0;
@@ -21,9 +21,9 @@
 			return;
 		}
 		if(blockCount<totalBlocks/2)
-		blockNo=Math.floor(Math.random()*11)+1;
+		blockNo=Math.floor(Math.random()*11)+1;		//Generate random number of blocks until half the blocks are hidden
 		else
-		blockNo=(i++)%12;
+		blockNo=(i++)%12;						//Generate sequential number otherwise, It is done so that it takes no large time to hide all blocks
 		if(!isFaded[blockNo]){
 			isFaded[blockNo]=true;
 			color[blockNo]="transparent";
@@ -42,7 +42,7 @@
 		width+="%";
 		$("#centerBar").css("width",width);
 	}
-	function setBackground(){
+	function setBackground(){	//sets background gradient
 		$("#centerBar").css("background-image","-moz-linear-gradient(left,"+color[0]+","+color[0]+" "+percent[0]+","+color[1]+" "+percent[0]+","+color[1]+" "+percent[1]+","+color[2]+" "+percent[1]+","+color[2]+" "+percent[2]+","+color[3]+" "+percent[2]+","+color[3]+" "+percent[3]+","+color[4]+" "+percent[3]+","+color[4]+" "+percent[4]+","+color[5]+" "+percent[4]+","+color[5]+" "+percent[5]+","+color[6]+" "+percent[5]+","+color[6]+" "+percent[6]+","+color[7]+" "+percent[6]+","+color[7]+" "+percent[7]+","+color[8]+" "+percent[7]+","+color[8]+" "+percent[8]+","+color[9]+" "+percent[8]+","+color[9]+" "+percent[9]+","+color[10]+" "+percent[9]+","+color[10]+" "+percent[10]+","+color[11]+" "+percent[10]+","+color[11]+" "+percent[11]+")");
 	}
 		$(document).ready(function(){
@@ -53,7 +53,7 @@
 				$("#feeds").removeAttr("disabled");
 				setBackground();
 				$("#centerBar ul li").css("opacity","0");
-				HidingId=setInterval(function(){makeHide();},100);
+				HidingId=setInterval(function(){makeHide();},100);		//The animation of hiding blocks
 				leftSideBarExtended=true;
 			}
 		});
